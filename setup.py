e893d2ef8c548404cdea9095f72e80ca4b14a45d
Lucas D. Wittwer <wittwer.lucas@gmail.com>
2021-11-11 11:16:21 +0100

    fix: added missing PyQt5 dependency

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     packages=find_packages(),
     install_requires=["numpy",
                       "matplotlib",
-                      "jupyter"
+                      "jupyter",
+                      "PyQt5"
                       ]
 )
